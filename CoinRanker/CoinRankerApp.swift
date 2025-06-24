796bcb6542a4b9738354edc271fd7b8b90929cf9
Felix-Kariuki <felixmurimi280@gmail.com>
2025-06-24 20:35:10 +0300

    Initial Commit

---
+++ CoinRanker/CoinRankerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinRankerApp.swift
+//  CoinRanker
+//
+//  Created by Felix kariuki on 24/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinRankerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
